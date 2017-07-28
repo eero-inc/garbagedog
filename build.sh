97d35872dcf125f3910370961128f3ee1a88c790
Will Bertelsen <willb@eero.com>
2017-07-28 16:54:05 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 mkdir -p ./dist
 VERSION=`python setup.py  --version`
-ARTIFACT=./dist/garbagedog-$OSTYPE-$VERSION.pex
-env PEX_VERBOSE=1 pex . -r requirements.txt -o $ARTIFACT -c garbagedog
-echo Built to $ARTIFACT
+
+OS=$OSTYPE
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="osx"
+fi
+
+ARTIFACT=./dist/garbagedog-${OS}-${VERSION}.pex
+env PEX_VERBOSE=1 pex . -r requirements.txt -o ${ARTIFACT} -c garbagedog
+echo Built to ${ARTIFACT}
