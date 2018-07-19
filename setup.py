bc68fceacf63bd7e2a0cac16ddac3e28ff5c9c51
Will Bertelsen <willb@eero.com>
2018-07-19 13:48:08 -0700

    Version 0.0.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='garbagedog',
-    version='0.0.11',
+    version='0.0.12',
     description='Parse JVM gc.logs and emit stats over dogstatsd',
     author='Will Bertelsen',
     author_email='willb@eero.com',
