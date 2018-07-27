ed4619954c3418b8f3ca806e08a956781d822c4e
Daniel Cash <dcash@eero.com>
2018-07-27 13:57:41 -0700

    Run all tests with test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-echo "Running PEP8" && pep8 ./garbagedog --ignore=E501,E701 && echo "Running mypy" && mypy --ignore-missing-imports --no-warn-no-return ./garbagedog && echo "Good to go!"
+echo "Running PEP8" && pep8 ./garbagedog --ignore=E501,E701 && echo "Running mypy" && mypy --ignore-missing-imports --no-warn-no-return ./garbagedog && echo "PEP8/mypy passed!" \
+    && echo "Running Python tests" && pytest -v && echo "Python tests passed!"
