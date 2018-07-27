f63cc05e6a9dd8c40fb1d0447cf5ec7620479dbf
Will Bertelsen <willb@eero.com>
2018-07-27 16:45:13 -0700

    Switch from PEP8 to pycodestyle (#18)
    * Switch from PEP8 to pycodestyle
    
    * also add pip cache

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Running PEP8" && pep8 ./garbagedog --ignore=E501,E701 && echo "Running mypy" && mypy --ignore-missing-imports --no-warn-no-return ./garbagedog && echo "PEP8/mypy passed!" \
+echo "Running pycodestyle" && pycodestyle ./garbagedog --ignore=E501,E701,W605 && echo "Running mypy" && mypy --ignore-missing-imports --no-warn-no-return ./garbagedog && echo "pycodestyle/mypy passed!" \
     && echo "Running Python tests" && pytest -v && echo "Python tests passed!"
